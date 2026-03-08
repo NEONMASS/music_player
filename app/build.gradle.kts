e0bbb2752c10cdbdb0bb6e4247e7be57e68ebf57
NEONMASS <neonmass@example.com>
2026-03-08 21:33:53 +0530

    Sync Java and Kotlin compilers to JVM 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.neo.musicplayer"
     compileSdk = 34
+
     defaultConfig {
         applicationId = "com.neo.musicplayer"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
