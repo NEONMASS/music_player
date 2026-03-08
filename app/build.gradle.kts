3e1218a4d4a1daee3eb542411db87780164bcd35
NEONMASS <neonmass@example.com>
2026-03-08 21:17:50 +0530

    Setup CI pipeline and base Android files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-
 android {
     namespace = "com.neo.musicplayer"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.neo.musicplayer"
         minSdk = 26
@@ -14,29 +12,4 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-
-    buildFeatures {
-        compose = true
-    }
-    
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
-    implementation(composeBom)
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
 }
