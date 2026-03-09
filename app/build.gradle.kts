7c6751035365daa8be0c4b4f03fef07927ec8bb9
NEONMASS <neonmass@example.com>
2026-03-09 12:26:59 +0530

    ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Room (Local SQLite Database)
     val roomVersion = "2.6.1"
